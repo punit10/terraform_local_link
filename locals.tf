1bbf18fc187fce1a96ea641f35b61e0534ace0c4
Punit Sharma <punitcs0067@gmail.com>
2024-11-20 11:37:16 +0530

    Added more resource s3, alb to take the website from s3 and iam policies

--- locals.tf
+++ locals.tf
@@ -6,4 +6,12 @@ locals {
     project      = "${var.company}-${var.project}"
     billing_code = var.billing_code
   }
+
+  s3_bucket_name = "local-link-app-${random_integer.s3.result}"
+}
+
+# It wll add a random number while creating s3
+resource "random_integer" "s3" {
+  min = 10000
+  max = 99999
 }
